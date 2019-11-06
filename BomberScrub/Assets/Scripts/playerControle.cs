c819010848cc1a578dcd08c448add7195312b57c
katarzyna19 <32496305+katarzyna19@users.noreply.github.com>
2019-11-06 13:35:48 +0100

    poruszanie sie obiektu po planszy

---
+++ BomberScrub/Assets/Scripts/playerControle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControle : MonoBehaviour
+{
+    public float speed = 10f;
+    float frontBack;
+    float leftRight;
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frontBack = Input.GetAxis("Vertical") * speed;
+        leftRight = Input.GetAxis("Horizontal") * speed;
+        Vector3 playerCon = new Vector3(leftRight, 0, frontBack);
+        cc.Move(playerCon * Time.deltaTime);
+    }
+}
