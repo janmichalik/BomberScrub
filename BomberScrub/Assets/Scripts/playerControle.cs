c76c56d4ebb18e828e1c078acefd618a60a152ad
Michał Ptak <mptakns@gmail.com>
2019-11-07 17:30:27 +0100

    Added audio files and static obstacles.

--- BomberScrub/Assets/Scripts/playerControle.cs
+++ BomberScrub/Assets/Scripts/playerControle.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class playerControle : MonoBehaviour
 {
+
+    private void Start()
+    {
+       /* int direction = 1;
+        GameObject prefab = Resources.Load ("Block") as GameObject;
+        for(int i=1; i<5; ++i)
+        {
+            for(int j=1; j<5; ++j)
+            {
+                if (i % 2 == 0)
+                    direction = -1;
+                else
+                    direction = 1;
+                GameObject go = Instantiate(prefab);
+                go.transform.position = new Vector3(4 * i * direction, 15 / 10, 8 * j * direction);
+            }
+        }*/
+    }
+
     public float speed = 10f;
     float frontBack;
     float leftRight;
