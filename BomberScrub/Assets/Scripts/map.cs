66e5193ecb3fb9571f2a88cfd609cbe15ffb12fe
michalptak <26803765+michalptak@users.noreply.github.com>
2019-11-13 21:33:07 +0100

    Generating static elements with script.

--- BomberScrub/Assets/Scripts/map.cs
+++ BomberScrub/Assets/Scripts/map.cs
@@ -7,20 +7,15 @@ public class map : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /* int direction = 1;
-        GameObject prefab = Resources.Load ("Block") as GameObject;
-        for(int i=1; i<5; ++i)
+        GameObject block = Resources.Load ("Block") as GameObject;
+        for(int i=1; i<17; ++i)
         {
-            for(int j=1; j<5; ++j)
+            for(int j=1; j<9; ++j)
             {
-                if (i % 2 == 0)
-                    direction = -1;
-                else
-                    direction = 1;
-                GameObject go = Instantiate(prefab);
-                go.transform.position = new Vector3(4 * i * direction, 15 / 10, 8 * j * direction);
+                GameObject go = Instantiate(block);
+                go.transform.position = new Vector3(6 * i, 15 / 10, 8 * j);
             }
-        }*/
+        }
     }
 
     // Update is called once per frame
