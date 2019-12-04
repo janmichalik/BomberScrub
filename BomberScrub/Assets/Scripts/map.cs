9754276a6ffdfd78da94e6a5fd394146898a078c
michalptak <26803765+michalptak@users.noreply.github.com>
2019-12-04 22:50:12 +0100

    Add destroyable elements generation.

--- BomberScrub/Assets/Scripts/map.cs
+++ BomberScrub/Assets/Scripts/map.cs
@@ -10,18 +10,45 @@ public class map : MonoBehaviour
     int whichCamera = 1;
     public Camera m_CameraTwo;
 
+
     // Start is called before the first frame update
     void Start()
     {
         GameObject block = Resources.Load ("Block") as GameObject;
-        for(int i=1; i<17; ++i)
+        GameObject crate = Resources.Load("Crate") as GameObject;
+        for (int i=1; i<17; ++i)
         {
             for(int j=1; j<9; ++j)
             {
+                // generowanie stalych elementow
                 GameObject go = Instantiate(block);
-                go.transform.position = new Vector3(6 * i, 15 / 10, 8 * j);
+                go.transform.position = new Vector3(6 * i - 1, (float)1.5, 8 * j);
+
+                if(j > 1)
+                {
+                    // generowanie zniszczalnych elementw
+                    // TODO: zrobic z tego funkcje
+                    GameObject box = Instantiate(crate);
+                    box.transform.position = new Vector3(6 * i - 1, (float)1.5, (float)3.9 + 8 * (j - 1));
+                    box.transform.localScale -= new Vector3((float)0.2, (float)0.2, (float)0.2);
+                    Renderer rend = box.GetComponent<Renderer>();
+                    rend.material.color = Color.blue;
+                }
+            }
+
+            for(int j=1; j<18; ++j)
+            {
+                if (i < 16)
+                {
+                    GameObject box = Instantiate(crate);
+                    box.transform.position = new Vector3(6 * i + 2, (float)1.5, (float) 3.9 * j);
+                    box.transform.localScale -= new Vector3((float) 0.2, (float)0.2, (float)0.2);
+                    Renderer rend = box.GetComponent<Renderer>();
+                    rend.material.color = Color.blue;
+                }
             }
         }
+
 
         // zaminicjalizowanie zmiennych kamery
         foreach (Camera c in Camera.allCameras)
