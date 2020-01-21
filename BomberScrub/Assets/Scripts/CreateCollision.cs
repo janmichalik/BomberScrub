2df217f01ac015da7f87c8aca7108709ab5ee320
katarzyna19 <32496305+katarzyna19@users.noreply.github.com>
2020-01-21 12:53:46 +0100

    Player explosion.

--- BomberScrub/Assets/Scripts/CreateCollision.cs
+++ BomberScrub/Assets/Scripts/CreateCollision.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreateCollision : MonoBehaviour
 {
-
+    public float delayTime = 3f;
     public void OnParticleCollision(GameObject other)
     {
         if(other.tag == "crate")
         {
             Destroy(other, 2f);
         }
+        if (other.tag == "player1")
+        {
+            Destroy(other, 2f);
+            Invoke("DelayedAction", delayTime);
+        }
+    }
+    void DelayedAction()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene("Menu");
+
     }
 
 }
