e971ad11b30cc0015093bc5a80195783f0b85831
Michał Ptak <26803765+michalptak@users.noreply.github.com>
2020-01-21 09:53:44 +0100

    Enabled main theme audio. Added bomb particle collision detection.

---
+++ BomberScrub/Assets/Scripts/CreateCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCollision : MonoBehaviour
+{
+
+    public void OnParticleCollision(GameObject other)
+    {
+        if(other.tag == "crate")
+        {
+            Destroy(other, 2f);
+        }
+    }
+
+}
